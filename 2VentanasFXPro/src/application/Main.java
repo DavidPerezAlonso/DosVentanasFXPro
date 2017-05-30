f5527604962d236e9d2d311c3853ee521af2a6b5
DavidPerezAlonso <david.perez.uem@gmail.com>
2017-05-30 09:03:58 +0200

    Comentarios

--- 2VentanasFXPro/src/application/Main.java
+++ 2VentanasFXPro/src/application/Main.java
@@ -23,6 +23,11 @@ public class Main extends Application {
 
     }
 
+    /* En un proyecto JavaFX el main llama al launch que a su vez llama a start */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     /*
      * cargamos la ventana principal
      */
@@ -44,9 +49,6 @@ public class Main extends Application {
         }
    }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     /* Este método es llamado cuando se presiona el botón de la ventana principal
      * Lo llama el controlador de la vista principal
@@ -55,13 +57,16 @@ public class Main extends Application {
         try {
             FXMLLoader loader = new FXMLLoader(Main.class.getResource("../vista/VentanaDos.fxml"));
             AnchorPane ventanaDos = (AnchorPane) loader.load();
+            /* Creamos la segunda ventana como otro stage */
             Stage ventana = new Stage();
             ventana.setTitle("Venta Dos");
             ventana.initOwner(stagePrincipal);
             Scene scene = new Scene(ventanaDos);
             ventana.setScene(scene);
+           
             VentanaDosController controller2 = loader.getController();
             controller2.setStagePrincipal(ventana);
+            
             ventana.show();
 
         } catch (Exception e) {
